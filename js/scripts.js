2c1a69854fba5aaf3d38223ed09961e8ab110aa8
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-08-23 13:19:36 -0700

    add if/if else statements to js function, add return to user

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,28 @@
-// Business Logic
+/// Business Logic
 function beepTranslator(localUserNumber) {
   var outputArray = [];
   for (var i = 0; i < localUserNumber; i++) {
-    outputArray.push(i);
+    if (i.toString().includes("3")) {
+      outputArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
+    } else if (i.toString().includes("2")) {
+      outputArray.push("Boop!");
+    } else if (i.toString().includes("1")) {
+      outputArray.push("Beep!");
+    } else {
+      outputArray.push(i.toString());
+    }
   }
+  return outputArray;
 }
 
 
 
 /// Front-End Logic
+
 $(document).ready(function() {
-  $("form#userNumber").submit(function() {
+  $("#userNumber").submit(function() {
     event.preventDefault();
     var userNumber = parseInt($('input#userNumber').val());
-    console.log(userNumber);
-
+    $("#result").text(beepTranslator (userNumber));
   });
 });
