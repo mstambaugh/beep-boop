9c72e8b9ef655dc3baa273850167f5385166f05b
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-08-23 13:57:07 -0700

    add username div and insert in function

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 /// Business Logic
-function beepTranslator(localUserNumber) {
+function beepTranslator(localUserNumber, localUserName) {
+  alert(localUserNumber);
   var outputArray = [];
   for (var i = 0; i < localUserNumber; i++) {
     if (i.toString().includes("3")) {
-      outputArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
+      outputArray.push("I'm sorry, " + localUserName + ", I'm afraid I can't do that.");
     } else if (i.toString().includes("2")) {
       outputArray.push("Boop!");
     } else if (i.toString().includes("1")) {
@@ -22,7 +23,9 @@ function beepTranslator(localUserNumber) {
 $(document).ready(function() {
   $("#userNumber").submit(function() {
     event.preventDefault();
-    var userNumber = parseInt($('input#userNumber').val());
-    $("#result").text(beepTranslator (userNumber));
+    var userName = $("#userName").val();
+    console.log(typeof(userName));
+    var userNumber = parseInt($("#userNumber").val());
+    $("#result").text(beepTranslator(userNumber, userName));
   });
 });
